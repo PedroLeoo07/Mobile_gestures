31d96440e60980867022ee379daf350af749888d
Leonardo Oliveira <leonardodeoliveira.tds1.senai@gmail.com>
2025-04-22 15:48:35 -0300

    correção de cores

--- screens/Cintra.js
+++ screens/Cintra.js
@@ -23,7 +23,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "lightblue",
+    backgroundColor: "lightgreen",
   },
   image: {
     width: 300,
--- screens/Oliveira.js
+++ screens/Oliveira.js
@@ -10,7 +10,7 @@ export default function Oliveira() {
         <Image
           style={styles.image}
           source={{
-            uri: "https://www.giantfreakinrobot.com/wp-content/uploads/2023/05/leonardo-dicaprio-point.png",
+            uri: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fbr.pinterest.com%2Fpin%2Fmessi-careca--691865561521418523%2F&psig=AOvVaw3lFY5SN9RK-h7RRF1F4DQ0&ust=1745433968003000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCKjCw9mm7IwDFQAAAAAdAAAAABAE",
           }}
         />
       </TouchableOpacity>
@@ -23,7 +23,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "yellow",
+    backgroundColor: "blue",
   },
   image: {
     width: 300,
